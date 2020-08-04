0887e6b487551fd3bbae72f1d8e128f1496d6b45
Megan Horsfield <meganthadev@gmail.com>
2020-08-04 20:27:28 +0000

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,12 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+let record 
+
+function superbowlWin(record) {
+  const results = record.find(function(object) {
+    return object.result === 'W'
+  })
+  return results === undefined ? undefined : results.year
+}
